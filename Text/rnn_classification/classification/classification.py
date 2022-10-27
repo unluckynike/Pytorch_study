c9472f29d208cd61fffcc149e3bf6e9b65fd9bc2
WuLiZeng <2230432084@qq.com>
2022-10-27 20:33:25 +0800

    姓名分类

---
+++ Text/rnn_classification/classification/classification.py
@@ -0,0 +1,10 @@
+
+'''
+@Project ：Pytorch_study 
+@File    ：classification.py.py
+@Author  ：hailin
+@Date    ：2022/10/27 20:32 
+@Info    : https://pytorch.org/tutorials/intermediate/char_rnn_classification_tutorial.html
+         rnn 姓名分类
+
+'''
